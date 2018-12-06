49be45c9fc639bc7629bb152c7f65a1c92cd96a5
Tatyana Milanova <tanyta78@gmail.com>
2018-12-06 18:32:09 +0200

    AutoMapper added. Closes #38

---
+++ Services/DrivingSchoolWebApp.Services.Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace DrivingSchoolWebApp.Services.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IMapperConfigurationExpression configuration);
+    }
+}

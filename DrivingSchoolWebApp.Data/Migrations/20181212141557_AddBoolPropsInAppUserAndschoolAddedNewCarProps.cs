23e029a3cd4cc2f602058a201f775730de47a1dd
Tanya Milanova <tanyta78@gmail.com>
2018-12-12 16:18:17 +0200

    Add some new props to db models and make migration.

---
+++ DrivingSchoolWebApp.Data/Migrations/20181212141557_AddBoolPropsInAppUserAndschoolAddedNewCarProps.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DrivingSchoolWebApp.Data.Migrations
+{
+    public partial class AddBoolPropsInAppUserAndschoolAddedNewCarProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Schools",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "InUse",
+                table: "Cars",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Make",
+                table: "Cars",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Transmission",
+                table: "Cars",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VIN",
+                table: "Cars",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Schools");
+
+            migrationBuilder.DropColumn(
+                name: "InUse",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Make",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Transmission",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "VIN",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "AspNetUsers");
+        }
+    }
+}

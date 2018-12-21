4f590f366c997e63de534e5722c0cee8d09dff7a
Tatyana Milanova <tanyta78@gmail.com>
2018-12-22 01:05:20 +0200

    Added certificate database model. closes #5

---
+++ DrivingSchoolWebApp.Data.Models/Certificate.cs
@@ -0,0 +1,23 @@
+﻿namespace DrivingSchoolWebApp.Data.Models
+{
+    using System;
+    using Common;
+
+    public class Certificate : BaseModel<int>
+    {
+        public Certificate()
+        {
+            this.IssueDate=DateTime.UtcNow;
+        }
+
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+
+        public DateTime IssueDate { get; set; }
+    }
+}
---
+++ DrivingSchoolWebApp.Data/Migrations/20181221215617_AddedCertificateModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DrivingSchoolWebApp.Data.Migrations
+{
+    public partial class AddedCertificateModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

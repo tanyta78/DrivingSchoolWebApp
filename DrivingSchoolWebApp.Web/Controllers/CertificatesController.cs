50a9e087c211ffe176d4d1573c70f5d88e33b9eb
Tatyana Milanova <tanyta78@gmail.com>
2018-12-30 10:25:50 +0200

    Certificate Controller completed

---
+++ DrivingSchoolWebApp.Web/Controllers/CertificatesController.cs
@@ -0,0 +1,62 @@
+﻿namespace DrivingSchoolWebApp.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Claims;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.DataServices.Contracts;
+    using Services.Models.Certificate;
+    using Services.Models.Customer;
+    using Services.Models.School;
+
+    public class CertificatesController : BaseController
+    {
+        private readonly ICertificateService certificateService;
+        private readonly ICustomerService customerService;
+        private readonly ISchoolService schoolService;
+
+        public CertificatesController(ICertificateService certificateService, ICustomerService customerService, ISchoolService schoolService)
+        {
+            this.certificateService = certificateService;
+            this.customerService = customerService;
+            this.schoolService = schoolService;
+        }
+
+        // GET: Certificates/All
+        public ActionResult All()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var certificates = new List<AllCertificatesViewModel>();
+
+            if (this.User.IsInRole("Customer"))
+            {
+                var customerId = this.customerService.GetCustomerByUserId<DetailsCustomerViewModel>(userId).Id;
+                certificates = this.certificateService.GetCertificatesByCustomerId<AllCertificatesViewModel>(customerId).ToList();
+            }
+            else if (this.User.IsInRole("School"))
+            {
+                var schoolId = this.schoolService
+                    .GetSchoolByManagerName<DetailsSchoolViewModel>(this.User.Identity.Name).Id;
+                certificates = this.certificateService.GetCertificatesBySchoolId<AllCertificatesViewModel>(schoolId).ToList();
+            }
+            return this.View(certificates);
+        }
+
+        //Get: Certificates/Details/3
+        public IActionResult Details(int id)
+        {
+            try
+            {
+                var certificate = this.certificateService.GetCertificateById<DetailsCertificateViewModel>(id);
+
+                return this.View(certificate);
+            }
+            catch (Exception e)
+            {
+                return this.View("_Error", e.Message);
+            }
+        }
+    }
+}
---
+++ Services/DrivingSchoolWebApp.Services.Models/Certificate/AllCertificatesVeiwModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DrivingSchoolWebApp.Services.Models.Certificate
+{
+    using System;
+    using Data.Models;
+    using Mapping;
+
+    public class AllCertificatesViewModel:IMapFrom<Certificate>
+    {
+
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string CustomerFullName { get; set; }
+
+        public int CourseId { get; set; }
+
+        public string CourseInfo { get; set; }
+        
+        public DateTime IssueDate { get; set; }
+
+    }
+}
